9a942d9f9b540072e5ae3b42a8662b892d04992f
hgottam <hgottam@asu.edu>
2022-10-27 16:55:19 -0700

    Changes related functional working of validation of input strings

--- src/main/java/org/example/Inputs.java
+++ src/main/java/org/example/Inputs.java
@@ -12,7 +12,6 @@ public class Inputs {
         System.out.println("Do you want to enter the input or read from file?");
         System.out.println("Select an option:");
         System.out.println("0-Enter input \n1- Read from file");
-        System.out.println();
         while(true) {
             int option = sc.nextInt();
             if (option == 0) {
@@ -27,39 +26,56 @@ public class Inputs {
                 break;
             } else {
                 System.out.println("Please enter valid option:");
-                option = sc.nextInt();
             }
         }
     }
     public void keyboardInput(){
-        String str="testString";
+        Scanner sc=new Scanner(System.in);
         Validation validate=new Validation();
-        validate.validateString(str);
-//        while(Integer.parseInt(str)!=-1){
-//            Validation validate=new Validation();
-//            validate.validateString(str);
-//            str="-1";
-//        }
+        System.out.println("Enter String:");
+        String str=sc.nextLine();
+        while(!str.equals("-1")){
+            if(!validate.validateString(str))
+                System.out.println("===Invalid String===");
+
+            System.out.println("enter again");
+            str=sc.nextLine();
+        }
     }
     public void fileInput() throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
         File f=new File("./src/main/resources/urinals.txt");
-        if(hasFileContent(f))
-            System.out.println("File has content");
-        else
+        if(!hasFileContent(f)){
             System.out.println("File doesnt have content");
+            return;
+        }
+
+        validateFileInputString(f);
+
+
+
         bw.flush();
     }
 
     public boolean hasFileContent(File f) throws IOException{
         //File f=new File("./src/main/resources/urinals.txt");
         BufferedReader in = new BufferedReader(new FileReader(f));
-        String line = "";
         int c=-1;
-        while((line=in.readLine())!=null)
+        while((in.readLine())!=null)
             c++;
         return c>=0;
+    }
+
+    public void validateFileInputString(File f) throws  IOException{
+        BufferedReader in = new BufferedReader(new FileReader(f));
+        String line=in.readLine();
+        Validation validate=new Validation();
+        while((line)!=null){
+            if(!validate.validateString(line))
+                System.out.println("===Invalid String===");
+            line=in.readLine();
+        }
     }
 
 
--- src/main/java/org/example/Validation.java
+++ src/main/java/org/example/Validation.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Validation {
     public boolean validateString(String str){
-        return str.charAt(0) == '1' || str.charAt(0) == '0';
+        boolean valid=true;
+        for(int i=0;i<str.length()-1;i++){
+            if(str.charAt(i)!='0'&& str.charAt(i)!='1' ){
+                valid=false;
+                break;
+            }
+            else if(str.charAt(i)=='1') {
+                if (str.charAt(i + 1) == '1') {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        if(str.charAt(str.length()-1)!='0' && str.charAt(str.length()-1)!='1')
+            valid=false;
+        return valid;
     }
 }
